826ef470640187b9757644f298e43d71f57e693b
kyokokurihara <kyokokurihara@KyokoK.local>
2026-03-31 00:18:53 +0900

    Add script

---
+++ analysis/manuscript_2026/Fig4/Fig4_table_taxonkit.sh
@@ -0,0 +1,9 @@
+conda activate taxonkit
+
+path_out=/Users/kyokokurihara/iLab/itolab_backup/backup-latest/Lab/projects/2507blastx/output/251111_3921_samples_external_final_test/
+db_path=/Users/kyokokurihara/iLab/itolab_backup/backup-latest/Lab/db/taxdmp
+
+cd "$path_out"
+
+cat taxids.txt | taxonkit lineage --data-dir "$db_path" | taxonkit reformat -f "{d}" --lineage-field 2 --data-dir "$db_path" -F | taxonkit reformat -f "{f};{g};{s}" --lineage-field 2 --data-dir "$db_path" -F | cut -f 1,3,4 > ./taxids_lineage.txt
+cat taxids.txt | taxonkit lineage --data-dir "$db_path" | taxonkit reformat -f "{d};{K};{p};{c};{o};{f};{g};{s}" --lineage-field 2 --data-dir "$db_path" -F | cut -f 1,3 > taxids_long.txt
