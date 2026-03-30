c6602f6877214f8321144fc7e45f6d8f9de69c0a
kyokokurihara <kyokokurihara@KyokoK.local>
2026-03-31 02:10:07 +0900

    Add script

---
+++ analysis/manuscript_2026/Fig5/Fig5J.R
@@ -0,0 +1,77 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(scales)
+
+# change working direcotry
+# setwd("/Users/kyokokurihara/Lab/projects/2507blastx/output/251225GSEA/chaphama_251216_MSigDB_Hallmark_2020_stat/dotplot_chaphama")  # stat
+# setwd("/Users/kyokokurihara/Lab/projects/2507blastx/output/251225GSEA/chaphama_260127_MSigDB_Hallmark_2020_stat/dotplot_chaphama_260127")  # 260127
+setwd("/Users/kyokokurihara/Lab/projects/2507blastx/output/251225GSEA/chaphama_260206_MSigDB_Hallmark_2020_stat_blastx_filtered/dotplot_chaphama_260209/")  # 260209
+
+# read table
+df_all <- read_csv("NES_termshared_top10_FDR05.csv") |>
+  mutate(
+    Term = gsub("-", "_", Term),
+    sig  = if_else(`FDR q-val` < 0.05, "FDR < 0.05", "ns"),
+    sig  = factor(sig, levels = c("FDR < 0.05", "ns"))
+  )
+
+# term order
+term_levels <- df_all |>
+  distinct(Term) |>
+  pull(Term)
+
+# project order
+project_levels <- df_all |>
+  distinct(Project) |>
+  pull(Project)
+# project_levels <- c("p1","p2","p3")
+
+df_all <- df_all |>
+  mutate(
+    Term    = factor(Term, levels = term_levels),
+    Project = factor(Project, levels = project_levels)
+  )
+
+global_size_max <- max(abs(df_all$mlog10_fdr), na.rm = TRUE)
+
+p <- ggplot(df_all, aes(
+  x = Project, y = Term,
+  size = abs(mlog10_fdr),
+  fill = NES,
+  color = sig
+)) +
+  geom_point(shape = 21, stroke = 0.6) +
+  scale_fill_gradient2(
+    low = "#2166AC", mid = "#F7F7F7", high = "#B2182B",
+    midpoint = 0, limits = c(-3, 3), oob = squish, name = "NES"
+  ) +
+  scale_size_continuous(
+    limits = c(0, global_size_max),
+    range = c(1, 6),
+    breaks = pretty(c(0, global_size_max), n = 3),
+    name = "-log10(FDR)"
+  ) +
+  scale_color_manual(
+    values = c("FDR < 0.05" = "black", "ns" = "white"),
+    breaks = c("FDR < 0.05"),
+    name = "FDR"
+  ) +
+  labs(x = NULL, y = NULL) +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(color = "black", angle = 90, hjust = 1, vjust = 1),
+    axis.text.y = element_text(color = "black"),
+    legend.title = element_text(size = 9),
+    legend.text  = element_text(size = 8),
+    plot.margin = margin(5, 5, 5, 5),
+    panel.grid.major = element_line(linewidth = 0.3),  # TODO
+    panel.grid.minor = element_line(linewidth = 0.3)  # TODO
+  ) +
+  guides(
+    fill = guide_colorbar(barheight = unit(20, "mm"), barwidth = unit(4, "mm"))
+  )
+
+ggsave("stat_dotplot_1panel.png", p, width = 4.2, height = 6.5, units = "in", dpi = 300)
+ggsave("stat_dotplot_1panel.pdf", p, width = 4.2, height = 6.5, units = "in")
